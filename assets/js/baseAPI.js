2f80481dd0f8e414e46751df6d6ab6688e9d01c7
web-star <2934490605@qq.com>
2021-04-12 16:40:22 +0800

    2021/4/12 16:40 用户信息认证完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,19 @@
 // 在发起请求前进行统一拦截对请求地址进行修改
 $.ajaxPrefilter(function(options) {
-    options.url = "http://api-breakingnews-web.itheima.net" + options.url
+    options.url = "http://api-breakingnews-web.itheima.net" + options.url;
+    // 判断请求路径是否包含my的请求路径，是的话就表示是带权限的请求
+    if(options.url.includes("my")) {
+        options.headers = {
+            Authorization: localStorage.token || "",
+        }
+        options.complete =  function({responseJSON:{message,status}}) {
+            console.log(message, status);
+            if (status === 1 && message === '身份认证失败！') {
+                // 1. 强制清空 token
+                localStorage.removeItem('token')
+                // 2. 强制跳转到登录页面
+                location.href = '/login.html'
+              }
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+$(function() {
+    // 调用获取用户信息
+    getUserBaseInfo();
+    // 给退出按钮绑定点击事件
+    $("#btn-logout").on("click", function() {
+        layer.confirm('确定退出吗?', {icon: 3, title:'提示'}, function(index){
+            location.href = "/login.html";
+            delete localStorage.token;
+            // localStorage.removeItem("token")
+            layer.close(index);
+          });
+    })
+})
+
+
+// 获取用户基本信息
+function getUserBaseInfo() {
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        // 带有my路径的请求需要在头部添加登陆时后台返回的token
+        // headers: {
+        //     Authorization: localStorage.token || ""
+        // },
+        success: function(res) {
+            if(res.status !== 0) {
+                return layer.msg(res.message);
+            } else if (res.status === 0) {
+                layer.msg(res.message);
+                renderAvatar(res.data);
+            }
+        }
+    });
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+    const name = user.nickname || user.username;
+    $("#welcome").html(`欢迎  ${name}`);
+    if(user.user_pic) {
+        $(".layui-nav-img").attr("src", user.user_pic).show();
+        $(".text-avatar").hide();
+    } else {
+        $(".layui-nav-img").hide();
+        const firstLetter = name[0].toUpperCase();
+        $(".text-avatar").html(firstLetter).show();
+    }
+}
